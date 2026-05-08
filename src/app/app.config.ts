d547b9d48028b2159faedb87568d83ce9aa120c0
satake-h <thiago.satake@hexing.com.br>
2026-05-08 16:08:55 -0300

    fix: update Angular CLI configuration and remove unused animation provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideAppKeycloak } from './core/keycloak/keycloak.config';
@@ -9,7 +8,6 @@ import { provideAppKeycloak } from './core/keycloak/keycloak.config';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideAnimationsAsync(),
     provideHttpClient(),
     provideRouter(routes),
     provideAppKeycloak()
